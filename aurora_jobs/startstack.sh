d1907361343263febcc655d1c507c9f7c813558d
Jake <i@am.so-aweso.me>
2015-04-10 14:33:42 +0100

    Don't fail on container removal

--- aurora_jobs/startstack.sh
+++ aurora_jobs/startstack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
-sudo docker rm -f consul registrator
+sudo docker rm -f consul registrator || true
 sudo docker run -d --name consul --net=host progrium/consul -server -bootstrap -ui-dir /ui -advertise 192.168.33.7
 sudo docker run -d -v /var/run/docker.sock:/tmp/docker.sock --name registrator -h $HOSTNAME registry.livewyer.com/registrator consul://172.17.42.1:8500
 aurora job create devcluster/cache/devel/redis redis.aurora
