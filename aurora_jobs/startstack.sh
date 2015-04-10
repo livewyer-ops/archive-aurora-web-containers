3afc52982879d49eaa248ba412dc569eef42c5a5
Jake <i@am.so-aweso.me>
2015-04-10 14:32:11 +0100

    Removed consul and registrator if they already exist

--- aurora_jobs/startstack.sh
+++ aurora_jobs/startstack.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash -xe
+sudo docker rm -f consul registrator
 sudo docker run -d --name consul --net=host progrium/consul -server -bootstrap -ui-dir /ui -advertise 192.168.33.7
 sudo docker run -d -v /var/run/docker.sock:/tmp/docker.sock --name registrator -h $HOSTNAME registry.livewyer.com/registrator consul://172.17.42.1:8500
 aurora job create devcluster/cache/devel/redis redis.aurora
